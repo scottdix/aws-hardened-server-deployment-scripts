bf383b1b345c2cc2757195c22e970f9ba92cd68b
Scott Dix <scott.dix@gmail.com>
2019-01-17 20:19:38 -0600

    Initial commit

---
+++ cis-aws-reverse-proxy.sh
@@ -0,0 +1,37 @@
+#!bin/bash
+
+# Update packages. Uncomment, if needed.
+#yum update -y 
+
+# Install Docker on Amazon Linux 2 using amazon-linux-extras
+amazon-linux-extras install docker -y
+
+# Start docker
+service docker start
+
+# Make sure docker starts upon reboot
+chkconfig docker on
+
+# NOTE: Remember that if you're using an autoscaling group with a load balancer,
+# you'll need to make sure your instances pass the health check. This is configured
+# in the docker-compose.yml file.
+
+# Open Port 80 in iptables, if needed. 
+#iptables -A INPUT -i eth0 -p tcp --dport 80 -m state --state NEW,ESTABLISHED -j ACCEPT
+#iptables -A OUTPUT -o eth0 -p tcp --sport 80 -m state --state ESTABLISHED -j ACCEPT
+
+# Open Port 443 in iptables, comment out if needed
+iptables -A INPUT -i eth0 -p tcp --dport 443 -m state --state NEW,ESTABLISHED -j ACCEPT
+iptables -A OUTPUT -o eth0 -p tcp --sport 443 -m state --state ESTABLISHED -j ACCEPT
+
+# Download docker compose file
+curl -L s3://  -o /usr/local/bin/docker-compose
+
+# Make file executable
+chmod +x /usr/local/bin/docker-compose
+
+# Run file
+./usr/local/bin/docker-compose
+
+
+
